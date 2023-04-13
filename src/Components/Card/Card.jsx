7226b75cdd317e7ef16e3ccc8929be2016a22dc6
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-04-13 20:15:49 +0530

    basic css shit

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,16 +7,23 @@ export const CardComponent = () => {
       <Row xs={1} md={2} lg={3} className="g-3 cont-card">
         {Array.from({ length: 6 }).map((_, idx) => (
           <Col>
-            <Card>
-              <Card.Img variant="top" src="https://img.freepik.com/free-photo/3d-render-modern-network-communications-background-with-plexus-design_1048-15596.jpg?w=1060&t=st=1681393926~exp=1681394526~hmac=dae26b9dc09a286c5068968e0567118fd4b4a740b8a960fb6264412b3d19d62a" />
+            <Card className="card">
+              <Card.Img
+                variant="top"
+                src="https://media.istockphoto.com/id/1392424035/photo/web3-next-generation-world-wide-web-blockchain-technology-with-decentralized-information.jpg?b=1&s=170667a&w=0&k=20&c=tX9HrPDwtcZ2YYEwjp0_0mBUmmwP2XdvDGPmeVe1FC8="
+                className="card-image"
+              />
               <Card.ImgOverlay>
                 <Card.Title className="card-title">Card title</Card.Title>
               </Card.ImgOverlay>
-              <Card.Text className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
+                <Card.Text className="card-text">
+                  <div className="card-title2">
+                  Card-Title
+                  </div>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
             </Card>
           </Col>
         ))}
