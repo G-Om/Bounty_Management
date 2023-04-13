bb3e2adc7e5cfa420ba39db84a58c11cb1fb9bec
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2023-04-13 12:20:25 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './Components/Demo/Demo.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Demo></Demo>
+      Hello World
     </div>
   );
 }
